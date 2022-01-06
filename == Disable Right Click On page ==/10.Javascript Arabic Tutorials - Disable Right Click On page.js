29de4a37ee931772db7151681b476f735b034cc3
Muhammad Salah <84187438+MoSalahBedair1@users.noreply.github.com>
2022-01-06 11:27:05 +0200

    Add files via upload

---
+++ == Disable Right Click On page ==/10.Javascript Arabic Tutorials - Disable Right Click On page.js
@@ -0,0 +1,14 @@
+/* Right Click = Context Menu */
+
+document.addEventListener('contextmenu', function (e) {
+
+    'use strict';
+
+    e.preventDefault();
+
+    console.log('You Clicked Right Click on Mouse');
+});
+
+
+
+

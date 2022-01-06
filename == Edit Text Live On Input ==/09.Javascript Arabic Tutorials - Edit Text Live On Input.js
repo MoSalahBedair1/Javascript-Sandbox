9f72a648e49c0ff39583effb03372a146a4f6b28
Muhammad Salah <84187438+MoSalahBedair1@users.noreply.github.com>
2022-01-06 11:26:28 +0200

    Add files via upload

---
+++ == Edit Text Live On Input ==/09.Javascript Arabic Tutorials - Edit Text Live On Input.js
@@ -0,0 +1,20 @@
+var myTitle = document.getElementById('title'),
+    myContent = document.getElementById('content'),
+    myTitleLive = document.getElementById('title-live'),
+    myContentLive = document.getElementById('content-live');
+
+
+myTitle.onkeyup = function () {
+    'use strict';
+
+    myTitleLive.textContent = this.value;
+
+};
+
+
+myContent.onkeyup = function () {
+    'use strict';
+
+    myContentLive.textContent = this.value;
+
+};
